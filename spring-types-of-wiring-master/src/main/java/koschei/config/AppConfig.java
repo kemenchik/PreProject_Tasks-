a07d592235a7c1fb26d9f03bf701d38867753c3e
kemenchik <cemennnnnnnnn@gmail.com>
2021-10-03 20:54:37 +0300

    2.1.4

---
+++ spring-types-of-wiring-master/src/main/java/koschei/config/AppConfig.java
@@ -0,0 +1,22 @@
+package koschei.config;
+
+import koschei.models.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "koschei")
+public class AppConfig {
+
+    @Bean
+    public   Island2 getIsland(Wood3 wood) {
+        return new Island2(wood);
+    }
+
+    @Bean
+    public Egg6 getEgg(Needle7 needle) {return new Egg6(needle);}
+
+    @Bean
+    public Deth8 getDeth() {return new Deth8();}
+}
